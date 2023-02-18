c3496c069781ef42fcc5d34daa92e8d9617bec8d
alphacpc <alphacpc@gmail.com>
2023-02-18 11:15:02 +0000

    GET and POST Methods working well on API category

--- Backend/src/main/java/com/africa/controller/CategoryController.java
+++ Backend/src/main/java/com/africa/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.africa.controller;
 
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,23 +10,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.africa.config.Needs;
+import com.africa.service.CategoryService;
 import com.africa.types.Category;
 
 @RestController
 @RequestMapping("api/v1/categories")
 public class CategoryController {
 	
-	@GetMapping
-	public Category request_all_categories() {
-		
-		Category category = new Category();
-		
-		category.setId_category(1L);
-		category.setTitle_category("Mafe dev");
-		category.setDesc_category("Lorem category");
-		
-		return category;
+	private final CategoryService categoryService;
+	 
+	public CategoryController(CategoryService categoryService) {
+		this.categoryService = categoryService;
 	}
+	
+	@GetMapping
+	public List<Category> request_all_categories() {
+		
+		return categoryService.get_categories();
+	}
+	
 	
 	@PostMapping("new")
 	public String request_create_category(@RequestBody Category category) {
@@ -32,7 +36,7 @@ public class CategoryController {
 		category.setCreatedAt_category(new Needs().render_datetime());
 		category.setLastUpdated_category(new Needs().render_datetime());
 		
-		return category.getTitle_category() + " created successfull.";
+		return categoryService.add_category(category);
 	}
 
 }
--- Backend/src/main/java/com/africa/service/CategoryService.java
+++ Backend/src/main/java/com/africa/service/CategoryService.java
@@ -1,5 +1,38 @@
 package com.africa.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import com.africa.repository.CategoryRepository;
+import com.africa.types.Category;
+
+@Service
 public class CategoryService {
+	
+	public final CategoryRepository categoryRepository;
+	
+	public CategoryService(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+	
+	public List<Category> get_categories(){
+		
+		return categoryRepository.findAll();
+				
+	}
+	
+	
+	public String add_category(Category category) {
+		
+		Optional<Category> categoryOptional = categoryRepository.findByTitleCategory(category.getTitle_category());
+		
+		if(categoryOptional.isPresent()) {
+			return "La categorie '"+ category.getTitle_category() + "' existe déjà !";
+		}
+		
+		categoryRepository.save(category);
+		return "Création avec succès de la catégorie '" + category.getTitle_category() + "'";
+	}
 
 }
