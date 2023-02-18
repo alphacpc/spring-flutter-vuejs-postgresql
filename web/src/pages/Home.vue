93ebad60accdc2b1ce9be2e04bd81c9badd94970
alphacpc <alphacpc@gmail.com>
2023-02-18 16:50:30 +0000

    Implemented Routes on vue app

---
+++ web/src/pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mainContainer" style="background:#282A3A">
+    <BannerComponent msg="Welcome to Your Vue.js App"/>
+    <div class="container mx-auto my-10">
+      <CategoriesComponent/>
+      <CardPersonComponent class="my-5"/>
+      <PaginateComponent/>
+    </div>
+  </div>
+</template>
+
+<script>
+import BannerComponent from "../components/Banner.vue"
+import CategoriesComponent from "../components/Categories.vue"
+import CardPersonComponent from "../components/CardPerson.vue"
+import PaginateComponent from "../components/Paginate.vue"
+
+export default {
+  name: 'Home',
+  components: {
+    BannerComponent,
+    CategoriesComponent,
+    CardPersonComponent,
+    PaginateComponent
+  }
+}
+</script>
+
+<style>
+  .mainContainer{
+    padding-bottom: 20px;
+  }
+</style>
