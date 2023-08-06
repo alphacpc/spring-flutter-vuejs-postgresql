b72d60131735caa0321bf821cc4870f9331221e0
alphacpc <alphacpc@gmail.com>
2023-08-06 19:30:25 +0000

    Correct bug on API project

---
+++ API/src/main/java/com/discover/Welcome.java
@@ -0,0 +1,16 @@
+package com.discover;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("welcome")
+public class Welcome {
+
+	@GetMapping
+	public String SayHello() {
+		return "HELLO API CONTROLLER, I'm working well !!!!";
+	}
+
+}
