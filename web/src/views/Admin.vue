5fcfe9ca02c932c150e046492d94a166c14f4c57
alphacpc <alphacpc@gmail.com>
2023-08-19 12:41:09 +0000

    Update by squeleton

--- web/src/views/Admin.vue
+++ web/src/views/Admin.vue
@@ -1,15 +1,9 @@
 <script setup>
-import { ref } from 'vue'
-const count = ref(0)
 </script>
 
 <template>
     <h1>Admin page</h1>
-  <button @click="count++">Count is: {{ count }}</button>
 </template>
 
 <style scoped>
-button {
-  font-weight: bold;
-}
 </style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,17 +1,10 @@
 <script setup>
-import { ref } from 'vue'
-import HelloWorld from '../components/HelloWorld.vue'
-const count = ref(0)
 </script>
 
 <template>
     <h1>Home page</h1>
-    <HelloWorld msg="Component HelloWord"/>
-  <button @click="count++">Count is: {{ count }}</button>
 </template>
 
 <style scoped>
-button {
-  font-weight: bold;
-}
+
 </style>
