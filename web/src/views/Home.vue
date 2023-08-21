ab1d137fbb0a89d63272be821623ce0ecbfa4fe3
alphacpc <alphacpc@gmail.com>
2023-08-21 19:56:00 +0000

    Updated banner categories

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -7,6 +7,17 @@ import Lieu from '../components/CardLieu.vue'
 
 <template>
 
+    <div className="banner-categories flex justify-center bg-green-500 p-2">
+        <router-link to="#" className="mr-10 bg-white text-green-400 font-normal text-xs border-[1px] border- min-w-[4rem] flex justify-center py-1 px-2  rounded-2xl"><fa icon="usrs"/>lac</router-link>
+        <router-link to="#" className="mr-10 bg-white text-green-400 font-normal text-xs border-[1px] border- min-w-[4rem] flex justify-center py-1 px-2  rounded-2xl"><fa icon="usrs"/>foret</router-link>
+        <router-link to="#" className="mr-10 bg-white text-green-400 font-normal text-xs border-[1px] border- min-w-[4rem] flex justify-center py-1 px-2  rounded-2xl"><fa icon="usrs"/>savane</router-link>
+        <router-link to="#" className="mr-10 bg-white text-green-400 font-normal text-xs border-[1px] border- min-w-[4rem] flex justify-center py-1 px-2  rounded-2xl"><fa icon="usrs"/>luxe</router-link>
+        <router-link to="#" className="mr-10 bg-white text-green-400 font-normal text-xs border-[1px] border- min-w-[4rem] flex justify-center py-1 px-2  rounded-2xl"><fa icon="usrs"/>bord de mer</router-link>
+        <router-link to="#" className="mr-10 bg-white text-green-400 font-normal text-xs border-[1px] border- min-w-[4rem] flex justify-center py-1 px-2  rounded-2xl"><fa icon="usrs"/>paysage</router-link>
+        <router-link to="#" className="mr-10 bg-white text-green-400 font-normal text-xs border-[1px] border- min-w-[4rem] flex justify-center py-1 px-2  rounded-2xl"><fa icon="usrs"/>art</router-link>
+        <router-link to="#" className="mr-10 bg-white text-green-400 font-normal text-xs border-[1px] border- min-w-[4rem] flex justify-center py-1 px-2  rounded-2xl"><fa icon="usrs"/>musique</router-link>
+    </div>
+
     <div className="px-8 flex justify-start flex-wrap py-8">
 
         <Lieu image="img1.jpg" alt="image1"/>  
@@ -17,10 +28,8 @@ import Lieu from '../components/CardLieu.vue'
         <Lieu image="img1.jpg" alt="image6"/>  
         <Lieu image="img1.jpg" alt="image6"/>  
         
-        
-
     </div>
-
+couple algo 
 </template>
 
 <style scoped>
