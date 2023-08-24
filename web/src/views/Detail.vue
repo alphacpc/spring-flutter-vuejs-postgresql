846e80f6188f56f52bea6fab51b575b7a58fa647
alphacpc <alphacpc@gmail.com>
2023-08-24 18:20:34 +0000

    Start design detail page

--- web/src/views/Detail.vue
+++ web/src/views/Detail.vue
@@ -2,7 +2,19 @@
 </script>
 
 <template>
-    <h1>Detail page</h1>
+    <div>
+        <div>
+            <img className="h-[20rem] w-full object-cover" src="./../assets/images/maps.jpg" alt={{alt}}/>
+        </div>
+        <div className="bg-red-200 px-8 py-4">
+            <div className="glo-container">
+                <div></div>
+                <div>
+                    details
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
